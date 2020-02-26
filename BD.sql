d9bc23a92736ea38c1e18c8b35f812894176c794
MalekTaktak <ghostmalek@hotmail.com>
2020-02-26 05:43:24 +0100

    Base de donnée finale
    Utilisez ça pour la validation Java

--- BD.sql
+++ BD.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 24, 2020 at 03:56 PM
+-- Generation Time: Feb 26, 2020 at 04:40 AM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -37,7 +37,17 @@ CREATE TABLE IF NOT EXISTS `aide` (
   `localisationAide` varchar(250) NOT NULL,
   `categorieAide` varchar(250) NOT NULL,
   PRIMARY KEY (`aideId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `aide`
+--
+
+INSERT INTO `aide` (`aideId`, `descriptionAide`, `localisationAide`, `categorieAide`) VALUES
+(1, 'malek', 'tunis', 'malek'),
+(2, 'desc', 'local', 'categ'),
+(3, 'DACCORD', 'JE FAIS CA', 'MERCI BERTIN'),
+(4, 'OKOKOK', 'OKOKOKOK', 'OKOKOKOK');
 
 -- --------------------------------------------------------
 
@@ -48,12 +58,22 @@ CREATE TABLE IF NOT EXISTS `aide` (
 DROP TABLE IF EXISTS `article`;
 CREATE TABLE IF NOT EXISTS `article` (
   `articleId` int(11) NOT NULL AUTO_INCREMENT,
+  `auteur` varchar(250) DEFAULT NULL,
+  `titre` varchar(250) DEFAULT NULL,
   `descriptionArticle` varchar(250) NOT NULL,
   `datePublication` date NOT NULL,
+  `img` varchar(250) DEFAULT NULL,
   `userId` int(11) NOT NULL,
   PRIMARY KEY (`articleId`),
   KEY `fk_useridarticle` (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `article`
+--
+
+INSERT INTO `article` (`articleId`, `auteur`, `titre`, `descriptionArticle`, `datePublication`, `img`, `userId`) VALUES
+(0, 'Malek Taktak', 'L\'intégration', 'C\'est un très grand problème Monsieur le PDG.', '2020-02-12', NULL, 22);
 
 -- --------------------------------------------------------
 
@@ -76,7 +96,17 @@ CREATE TABLE IF NOT EXISTS `beneficiaire` (
   `besoin` varchar(250) DEFAULT NULL,
   `nationalite` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`beneficiaireId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `beneficiaire`
+--
+
+INSERT INTO `beneficiaire` (`beneficiaireId`, `aideId`, `nomBeneficiaire`, `prenomBeneficiaire`, `email`, `dateNaissance`, `ville`, `telephone`, `adresseGPS`, `role`, `besoin`, `nationalite`) VALUES
+(1, 1, 'xdfdfg', 'fgfghgf', 'malek.taktak@esprit.tn', '2020-02-18', 'Tunis', 28328382, 'tunis', 'Refugie', NULL, 'tunisien'),
+(2, 2, 'Taktak', 'Malek', 'malek.taktak@esprit.tn', '2020-02-05', 'TUNIS', 984912492, 'STRING', 'Necessiteux', 'ARGENT', NULL),
+(3, 3, 'JUNIOR', 'BERTIN', 'malek.taktak@esprit.tn', '2020-02-05', 'TUNIS', 12321312, 'STRING', 'Necessiteux', 'SOMMEIL', NULL),
+(4, 4, 'STEPHANE', 'JORDY', 'malek.taktak@esprit.tn', '2020-02-08', 'TUNIS', 12345678, 'VFGHJ', 'Necessiteux', 'MANGER', NULL);
 
 -- --------------------------------------------------------
 
@@ -101,8 +131,8 @@ CREATE TABLE IF NOT EXISTS `commentaire` (
 --
 
 INSERT INTO `commentaire` (`userId`, `questionId`, `texteCommentaire`, `score`, `dateCommentaire`) VALUES
-(22, 5, 'Merci Mohamed, est ce que je peux le récupèrer demain à 10H du matin dans la buvette?', 0, '2020-02-23'),
-(23, 5, 'Salut Malek, j\'ai trouvé ton portefeuille, tu peux me contacter je te le rends.', 0, '2020-02-11');
+(22, 5, 'Merci Mohamed, est ce que je peux le récupèrer demain à 10H du matin dans la buvette?', 1, '2020-02-23'),
+(23, 5, 'Salut Malek, j`ai trouvé ton portefeuille, tu peux me contacter je te le rends.', 1, '2020-02-11');
 
 -- --------------------------------------------------------
 
@@ -122,15 +152,24 @@ CREATE TABLE IF NOT EXISTS `don` (
   `rueRefuge` varchar(250) DEFAULT NULL,
   `villeRefuge` varchar(250) DEFAULT NULL,
   `paysRefuge` varchar(250) DEFAULT NULL,
+  `longitude` double DEFAULT NULL,
+  `latitude` double DEFAULT NULL,
   `disponibiliteRefuge` varchar(250) DEFAULT NULL,
   `capaciteRefuge` int(11) DEFAULT NULL,
   `quantiteDonNature` int(11) DEFAULT NULL,
-  `dateDon` date NOT NULL,
-  `dateDebutRefuge` date NOT NULL,
-  `dateFinRefuge` date NOT NULL,
+  `dateDon` date DEFAULT NULL,
+  `dateDebutRefuge` date DEFAULT NULL,
+  `dateFinRefuge` date DEFAULT NULL,
   PRIMARY KEY (`donId`),
   KEY `fk_donUser` (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `don`
+--
+
+INSERT INTO `don` (`donId`, `userId`, `typeDon`, `montantDon`, `cibleDon`, `libelleDonNature`, `categorieDonNature`, `rueRefuge`, `villeRefuge`, `paysRefuge`, `longitude`, `latitude`, `disponibiliteRefuge`, `capaciteRefuge`, `quantiteDonNature`, `dateDon`, `dateDebutRefuge`, `dateFinRefuge`) VALUES
+(23, 23, 'Refuge', NULL, NULL, NULL, NULL, 'GGG', 'GG', 'GG', 23.7654, 77.009, '0', 12, NULL, NULL, '2020-02-28', '2020-02-29');
 
 -- --------------------------------------------------------
 
@@ -147,18 +186,18 @@ CREATE TABLE IF NOT EXISTS `evenement` (
   `heureEvenement` time NOT NULL,
   `porteeEvenement` varchar(250) NOT NULL,
   `prixEvenement` int(11) NOT NULL,
-  PRIMARY KEY (`evenementId`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+  `image` varchar(250) NOT NULL,
+  `userId` int(11) NOT NULL,
+  PRIMARY KEY (`evenementId`),
+  KEY `userId` (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `evenement`
 --
 
-INSERT INTO `evenement` (`evenementId`, `descriptionEvenement`, `lieuEvenement`, `dateEvenement`, `heureEvenement`, `porteeEvenement`, `prixEvenement`) VALUES
-(1, 'Visite des malades', 'Tunis', '2020-02-05', '22:42:00', 'Personnes agées', 0),
-(2, 'Visite des malades', 'Tunis', '2020-02-05', '22:44:00', 'Personnes agées', 0),
-(3, 'Visite des malades', 'Tunis', '2020-02-05', '22:44:00', 'Personnes agées', 0),
-(4, 'Visite des malades', 'Tunis', '2020-02-05', '22:44:00', 'Personnes agées', 0);
+INSERT INTO `evenement` (`evenementId`, `descriptionEvenement`, `lieuEvenement`, `dateEvenement`, `heureEvenement`, `porteeEvenement`, `prixEvenement`, `image`, `userId`) VALUES
+(5, 'Nouvel event', 'Ghazela', '2020-02-14', '01:58:00', 'Les jeunes de 18 ans', 0, 'C:UsersghostPictureshandshakegray.png', 23);
 
 -- --------------------------------------------------------
 
@@ -190,14 +229,14 @@ CREATE TABLE IF NOT EXISTS `question` (
   `userId` int(11) NOT NULL,
   PRIMARY KEY (`questionId`),
   KEY `userId` (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `question`
 --
 
 INSERT INTO `question` (`questionId`, `texteQuestion`, `dateQuestion`, `score`, `userId`) VALUES
-(5, 'Bonsoir tout le monde, auriez vous vu un portefeuille marron à ESPRIT Ghazela? Je l\'ai perdu aujourd\'hui dans la buvette.', '2020-02-11', 0, 22),
+(5, 'Bonsoir tout le monde, auriez vous vu un portefeuille marron à ESPRIT Ghazela? Je l`ai perdu aujourd`hui dans la buvette.', '2020-02-11', 3, 22),
 (15, 'Bonsoir, est ce quelqu\'un a perdu un portefeuille marron à ESPRIT? Je l\'a trouvé aujourd\'hui à la buvette.', '2020-02-17', 0, 23);
 
 -- --------------------------------------------------------
@@ -227,6 +266,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `userId` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(250) NOT NULL,
   `password` varchar(250) NOT NULL,
+  `profil` varchar(250) DEFAULT NULL,
   `nomUser` varchar(250) DEFAULT NULL,
   `prenomUser` varchar(250) DEFAULT NULL,
   `email` varchar(250) NOT NULL,
@@ -242,16 +282,15 @@ CREATE TABLE IF NOT EXISTS `user` (
   UNIQUE KEY `userId` (`userId`),
   UNIQUE KEY `login` (`login`,`email`),
   UNIQUE KEY `telephone` (`telephone`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`userId`, `login`, `password`, `nomUser`, `prenomUser`, `email`, `telephone`, `nomOrganisation`, `ville`, `rue`, `pays`, `domaine`, `role`, `accesShakeHub`) VALUES
-(22, 'Malek', '1234', NULL, NULL, 'malek.taktak@esprit.tn', NULL, NULL, NULL, NULL, NULL, NULL, 'admin', 1),
-(23, 'Mohamed', '1212', NULL, NULL, 'mohamed2@esprit.tn', NULL, NULL, NULL, NULL, NULL, NULL, 'admin', 1),
-(24, 'Ahmed', 'moncef', NULL, NULL, 'abdelkarim@esprit.tn', NULL, NULL, NULL, NULL, NULL, NULL, 'user simple', 1);
+INSERT INTO `user` (`userId`, `login`, `password`, `profil`, `nomUser`, `prenomUser`, `email`, `telephone`, `nomOrganisation`, `ville`, `rue`, `pays`, `domaine`, `role`, `accesShakeHub`) VALUES
+(22, 'Malek', '1234', 'file:///C:/Users/ghost/Pictures/malek.jpg', 'Taktak', 'Malek', 'malek.taktak@esprit.tn', NULL, NULL, NULL, NULL, NULL, NULL, 'admin', 1),
+(23, 'Mohamed', '1212', 'file:///C:/Users/ghost/Pictures/Woah.png', 'Daghbari', 'Mohamed Ali', 'mohamed@esprit.tn', NULL, NULL, NULL, NULL, NULL, NULL, 'User Simple', 1);
 
 -- --------------------------------------------------------
 
@@ -292,6 +331,12 @@ ALTER TABLE `commentaire`
 --
 ALTER TABLE `don`
   ADD CONSTRAINT `fk_donUser` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `evenement`
+--
+ALTER TABLE `evenement`
+  ADD CONSTRAINT `fk_evenementuserId` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `participation`
